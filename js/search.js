6cbaf526fa4287f150f04f059ea067cdd4b3d4ae
up201604948 <up201604948@fe.up.pt>
2019-03-10 21:32:05 +0000

    Search input fake results

--- js/search.js
+++ js/search.js
@@ -1,7 +1,36 @@
-(new IntersectionObserver(function(e,o){
+/**
+ * Navbar scroll transition.
+ */
+(new IntersectionObserver((e,o) => {
     if (e[0].intersectionRatio > 0){
         document.querySelector('nav').classList.remove('stuck');
     } else {
         document.querySelector('nav').classList.add('stuck');
     };
 })).observe(document.querySelector('.trigger'));
+
+/**
+ * Search fields onchange events.
+ */
+document.querySelectorAll('.dropdownField').forEach(element => {
+    element.addEventListener('click', () => {
+        document.querySelectorAll('*[data-field="'+element.dataset.field+'"]').forEach(element2 => {
+            element2.classList.toggle('btn-outline-primary');
+            element2.classList.toggle('btn-primary');
+        });
+    });
+});
+
+/**
+ * Search submit button event.
+ */
+document.getElementById('button-go').addEventListener('click', () => {
+    let query = document.getElementById('search-input').value;
+
+    if (query === "") {
+        document.getElementById('search-query').textContent = "Trending event";
+        return;
+    }
+
+    document.getElementById('search-query').textContent = query;
+});
