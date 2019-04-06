a642f504cb249f20fa459ce871ce7bc49935ee04
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2019-04-06 18:05:14 +0100

    Added trigger post_likes_count

--- database/triggers.sql
+++ database/triggers.sql
@@ -25,3 +25,32 @@ CREATE TRIGGER participations_count
     AFTER INSERT OR DELETE ON participations
     FOR EACH ROW
     EXECUTE PROCEDURE participations_count();
+
+
+--Trigger: post_likes_count
+
+DROP TRIGGER IF EXISTS post_likes_count ON post_likes;
+
+CREATE OR REPLACE FUNCTION post_likes_count() RETURNS TRIGGER AS
+$BODY$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        UPDATE posts
+        SET likes = likes + 1
+        WHERE New.post_id = posts.id;
+    END IF;
+
+    IF TG_OP = 'DELETE' THEN
+        UPDATE posts
+        SET likes = likes - 1
+        WHERE Old.post_id = posts.id;
+    END IF;
+    RETURN NEW;
+END
+$BODY$ 
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER post_likes_count
+    AFTER INSERT OR DELETE ON post_likes
+    FOR EACH ROW
+    EXECUTE PROCEDURE post_likes_count();
