da4ba5bee5f446b762d721a0108b726faedfee8a
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2019-04-21 23:40:30 +0100

    Started profile page

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,8 @@ Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
+
+
+//Profile
+
+Route::get('profile', 'ProfileController@profile');
