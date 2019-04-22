c5e4584a79c2bc8ff75efff0d2625da02b220305
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2019-04-22 16:33:57 +0100

    Auto stash before merge of "feature/profile" and "origin/proto"

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     
     public function show() {
-        return view('pages.profile');
+        if (!Auth::check()) return redirect('/login');
+
+        $events['joined'] = Auth::user()->participations('Participant')->get();
+        $events['joined']->map(function ($item, $key) { return $item->event(); });
+        $events['hosting']  = Auth::user()->participations('Host')->orderByDesc('date')->get();
+        $events['performing']  = Auth::user()->participations('Artist')->orderByDesc('date')->get();
+
+        echo $events['joined'];
+
+        return view('pages.profile',  $events);
     }
 
 }
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -14,20 +14,20 @@
 						</div>
 						<div class="row justify-content-center position-relative">
 							<div class="col-10 text-center">
-								<h3 class="card-title user-name">John Smith</h3>
+                            <h3 class="card-title user-name">{{Auth::user()->name}}</h3>
 							</div>
 						</div>
 						<div class="row justify-content-center mb-2">
 							<div class="col-10 text-center">
-								<p class="card-subtitle text-muted">@john_smith</p>
+								<p class="card-subtitle text-muted">{{Auth::user()->username}}</p>
 							</div>
 						</div>
 						<div class="row justify-content-center mb-4">
 							<div class="col-5 col-sm-4 text-right border-right pr-2">
-								<p class="card-text">123 followers</p>
+								<p class="card-text">{{Auth::user()->followers}} followers</p>
 							</div>
 							<div class="col-5 col-sm-4 text-left pl-0 ml-2">
-								<p class="card-text">124 following</p>
+								<p class="card-text">{{Auth::user()->following}} following</p>
 							</div>
 						</div>
 						<div class="row justify-content-center mb-3">
@@ -36,7 +36,7 @@
 									role="button" aria-expanded="false" aria-controls="mytickets">My tickets</a>
 							</div>
 							<div class="col text-left">
-								<a href="settings.html" class="card-link border-bottom">Settings</a>
+                            <a href="{{ url('/settings')}}" class="card-link border-bottom">Settings</a>
 							</div>
 						</div>
 					</div>
@@ -59,261 +59,17 @@
 							<div class="tab-pane fade show active" id="joined" role="tabpanel"
 								aria-labelledby="joined-tab">
 								<div class="row">
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event.html" class="col-12 event-link">
-															<h5 class="event-title">
-																NOS Primavera Sound 2019
-															</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event.html" class="col-12 event-link">
-															<h5 class="event-title">
-																NOS Primavera Sound 2019
-															</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event.html" class="col-12 event-link">
-															<h5 class="event-title">
-																NOS Primavera Sound 2019
-															</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow card-fade"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event.html" class="col-12 event-link">
-															<h5 class="event-title">
-																NOS Primavera Sound 2019
-															</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
+									@each('partials.activity_event_card', $joined, 'joinedEvent')
 								</div>
 							</div>
 							<div class="tab-pane fade" id="hosting" role="tabpanel" aria-labelledby="performing-tab">
 								<div class="row">
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event-host.html" class="col-12 event-link">
-															<h5 class="event-title">
-																NOS Primavera Sound 2019
-															</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event-host.html" class="col-12 event-link">
-															<h5 class="event-title">
-																NOS Primavera Sound 2019
-															</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
+                                        @each('partials.activity_event_card', $hosting, 'hostingEvent')
 								</div>
 							</div>
 							<div class="tab-pane fade" id="performing" role="tabpanel" aria-labelledby="performing-tab">
 								<div class="row">
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event.html" class="col-12 event-link">
-															<h5 class="event-title">NOS Primavera Sound 2019</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-
-									<div class="col-12 col-md-10 mx-auto mt-5">
-										<div class="activity-event-card d-flex align-items-center hover-shadow card-fade"
-											tabindex="-1">
-											<div class="w-25 h-100 overflow-hidden d-flex justify-content-center">
-												<img class="h-100" src="../assets/event-placeholder.png" alt="">
-											</div>
-											<div class="d-flex align-items-center w-75">
-												<div class="container-fluid">
-													<div class="row align-items-center">
-														<a href="event.html" class="col-12 event-link">
-															<h5 class="event-title">NOS Primavera Sound 2019</h5>
-														</a>
-
-														<div class="col-7 col-md-9 mt-4">
-															<div class="text-muted event-date">6 JUN 2019</div>
-															<div class="text-muted event-location">Parque da Cidade, Porto</div>
-														</div>
-
-														<div class="col-5 col-md-3 mt-4">
-															<button type="button"
-																class="btn btn-outline-primary joined-btn w-100">
-																Joined
-															</button>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
+                                        @each('partials.activity_event_card', $performing, 'performingEvent')
 								</div>
 							</div>
 						</div>
