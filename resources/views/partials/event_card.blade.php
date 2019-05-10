7ee32c5ebd38b8740db49fa1ce3ece081d9c38b0
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-05-10 01:44:20 +0100

    Search page partials templates created

---
+++ resources/views/partials/event_card.blade.php
@@ -0,0 +1,31 @@
+<div class="col-12 col-md-6 col-lg-4">
+    <a href="event.html" class="event-card mb-5 hover-shadow link" tabindex="-1">
+        <header class="w-100 position-relative event-header d-flex align-items-center">
+            <img src="../assets/event-placeholder.png" class="w-100" alt="">
+            <div class="position-absolute w-100 h-100 gradient-overlay"></div>
+            <h6 class="position-absolute event-title px-3">
+                NOS Primavera Sound 2019
+            </h6>
+        </header>
+        <article class="event-info py-2">
+            <div class="container-fluid">
+                <div class="row d-flex align-items-center mt-1">
+                    <i class="col-1 far fa-calendar-alt"></i>
+                    <div class="col-10">6 JUN 2019</div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row d-flex align-items-center mt-1">
+                    <i class="col-1 fas fa-map-marker-alt"></i>
+                    <div class="col-10">Parque da Cidade, Porto</div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row d-flex align-items-center mt-1">
+                    <i class="col-1 fas fa-ticket-alt"></i>
+                    <div class="col-10">50€</div>
+                </div>
+            </div>
+        </article>
+    </a>
+</div>
