bb17d702f0d8dabe1bc3b71bf10d8d6cef5f5d33
Tiago Fragoso <up201606040@fe.up.pt>
2019-06-04 18:58:09 +0100

    Invite requests UI proto

--- public/js/event_invites.js
+++ public/js/event_invites.js
@@ -16,24 +16,51 @@ document.querySelector('#invite-btn').addEventListener('click', async () => {
 });
 
 function populateModal(data) {
+	
 	const list = document.querySelector('#invite-list');
 	while (list.firstChild) {
 		list.firstChild.remove();
 	}
 	for (let user of data) {
 		list.appendChild(createEl(user));
+		list.appendChild(getHr());
 	}
 }
 
+function getHr() {
+	const hr_row = document.createElement('div');
+	hr_row.className = 'row';
+	hr_row.innerHTML = '<div class="col-12"><hr class="mb-1 mt-1"></div>';
+	return hr_row;
+}
+
 function createEl(user) {
+	const href = `/users/${user['id']}`;
 	const wrapper = document.createElement('div');
 	wrapper.className = 'row';
-	wrapper.innerHTML = '<div class="user-row col-12 d-flex flex-row justify-content-between"></div>';
-	const content = wrapper.querySelector('.user-row');
-	for (let p in user) {
-		const el = document.createElement('p');
-		el.textContent = user[p];
-		content.appendChild(el);
+	wrapper.innerHTML = `
+		<div class="user-row col-11 mx-auto d-flex flex-row justify-content-between align-items-center">
+			<div class="leftWrapper d-flex flex-row align-items-center">
+				<img class="rounded-circle rounded-circle border border-light mr-3" width="35" height="35"></img>
+				<div class="d-flex flex-column">
+					<a class="name font-weight-bold"></a>
+					<a class="username text-muted"></a>
+				</div>
+			</div>
+			<button>Invite</button>
+		</div>`;
+
+	const pic = wrapper.querySelector('.leftWrapper > img');
+	pic.setAttribute('src', '/assets/user.svg'); //change this?
+	pic.setAttribute('href', href);
+
+	if (!user['name']) {
+		wrapper.querySelector('.name').remove();
+	} else {
+		wrapper.querySelector('.name').textContent = user['name'];
+		wrapper.querySelector('.name').setAttribute('href', href);
 	}
+	wrapper.querySelector('.username').textContent = user['username']? '@' + user['username']: '';
+	wrapper.querySelector('.username').setAttribute('href', href);
 	return wrapper;
 }
--- resources/views/pages/event.blade.php
+++ resources/views/pages/event.blade.php
@@ -43,7 +43,7 @@
 				</div>
 				<!-- Invite requests modal -->
 				<div class="modal fade" id="inviteRequestsModal" tabindex="-1" role="dialog" aria-labelledby="inviteRequestsTitle" aria-hidden="true">
-					<div class="modal-dialog modal-dialog-centered" role="document">
+					<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
 						<div class="modal-content">
 							<div class="modal-header">
 								<h5 class="modal-title" id="inviteRequestsTitle">Invite users</h5>
@@ -66,7 +66,7 @@
 									</div>
 									<div class="row">
 										<div class="col-12">
-											<hr>
+											<hr class="mb-1">
 										</div>
 									</div>
 									<div id="invite-list" class="d-flex flex-column">
