61619652d9f292c525a469f11acc622d0fcfa6df
Rita Norinho <ritanorinho98@hotmail.com>
2019-06-05 09:24:46 +0100

    added button to delete account

--- resources/views/partials/profile_header.blade.php
+++ resources/views/partials/profile_header.blade.php
@@ -1,16 +1,19 @@
-@if(Auth::check() && Auth::user()->id != $user->id)
+@if(Auth::check())
 <div class="dropdown position-absolute more-button">
     <button class="btn btn-light border-light" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="fas fa-ellipsis-h"></i>
     </button>
    
     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-        @if(Auth::check() && !$user->banned)
+        @if(Auth::check() && !$user->banned && !$user->deleted)
         @if(Auth::check() && Auth::user()->is_admin)
         <a class="dropdown-item text-danger" id="ban-user-btn" href="#"> Ban user</a>
-        @else
+        @elseif(Auth::user()->id != $user->id)
         <a class="dropdown-item text-danger" id="report-user-btn" href="#"  data-toggle="modal" data-target="#exampleModal">
             Report user</a>
+        @elseif(Auth::user()->id == $user->id)
+        <a class="dropdown-item text-danger" id="delete-account-btn" href="#"  data-toggle="modal" data-target="#exampleModal">
+            Delete account</a>
         @endif
         @endif
     </div>
