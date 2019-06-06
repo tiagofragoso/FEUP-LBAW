d75d990631e70599877d1eef3ed674d5bab64d29
Rita Norinho <ritanorinho98@hotmail.com>
2019-06-06 18:16:50 +0100

    fixed css

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container email">
     <h1 class="my-3">Reset Password</h1>
     <hr>
     <section class="pb-3">
